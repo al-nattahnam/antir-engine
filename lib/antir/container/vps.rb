4c43542ed9f08c4e8b63f92734030e6cad42a891
Fernando Alonso <krakatoa1987@gmail.com>
2011-09-07 14:06:48 -0300

    Delegated XML handling of VPS to another class

--- lib/antir/container/vps.rb
+++ lib/antir/container/vps.rb
@@ -1,5 +1,6 @@
 require 'libvirt'
-require 'libxml'
+require 'vps/xml'
+require 'forwardable'
 
 # conn.domains
 # dom = conn.domains.first
@@ -16,61 +17,25 @@ module Antir
 
       attr_accessor :driver
 
+      extend Forwardable
+
       def initialize(driver = :openvz, create = true)
         @driver = driver
         # check valid driver: @@types.include?(driver)
-        vps_template = self.class.vps_template(@driver)
 
-        @xml = LibXML::XML::Document.new
-        @xml.root = vps_template
+        #id_disponible = self.class.max_id + 1
+        #self.id = id_disponible
+        #self.name = id_disponible
+        #self.ip = "10.10.1.#{id_disponible}"
 
-        id_disponible = self.class.max_id + 1
-        self.id = id_disponible
-        self.name = id_disponible
-        self.ip = "10.10.1.#{id_disponible}"
-
+        @xml = Antir::Container::VPS::XML.new
         self
       end
+      def_delegators :@xml, :id, :name, :uuid, :ip, :'id=', :'name=', :'uuid=', :'ip='
 
       def self.max_id
         conn = Libvirt::connect('openvz:///system')
         conn.domains.collect(&:id).max
-      end
-
-      def id=(id)
-        id_node = @xml.find('//domain')[0]
-        id_node['id'] = id.to_s
-      end
-      def id
-        id_node = @xml.find('//domain')[0]
-        id_node['id']
-      end
-
-      def name=(name)
-        name_node = @xml.find('//domain/name')[0]
-        name_node.content = name.to_s
-      end
-      def name
-        name_node = @xml.find('//domain/name')[0]
-        name_node.content
-      end
-
-      def uuid=(uuid)
-        uuid_node = @xml.find('//domain/uuid')[0]
-        uuid_node.content = uuid
-      end
-      def uuid
-        uuid_node = @xml.find('//domain/uuid')[0]
-        uuid_node.content
-      end
-
-      def ip=(ip)
-        ip_node = @xml.find('//domain/devices/interface/ip')[0]
-        ip_node['address'] = ip
-      end
-      def ip
-        ip_node = @xml.find('//domain/devices/interface/ip')[0]
-        ip_node['address']
       end
 
       def self.find(id)
@@ -80,7 +45,7 @@ module Antir
         xml = LibXML::XML::Parser.string(dom.xml).parse
         domain_xml = xml.find('//domain')[0]
 
-        vps = VPS.new
+        vps = Antir::Container::VPS.new
         vps.id = domain_xml['id']
         vps.uuid = domain_xml.find('//uuid')[0].content
         vps.name = domain_xml.find('//name')[0].content
@@ -93,8 +58,7 @@ module Antir
         #   name presence
 
         conn = Libvirt::connect('openvz:///system')
-        xml = @xml.find('//domain')[0].to_s(:indent => true)
-        conn.domains.create(xml)
+        conn.domains.create(@xml)
       end
 
       def stop
@@ -103,62 +67,11 @@ module Antir
       end
 
       def xml
-        # para debug solamente
-        @xml
-      end
-
-      def to_xml
-        @xml.find("//domain")[0].to_s(:indent => true)
+        @xml.to_xml
       end
 
       def self.drivers
         @@driver
-      end
-
-      private
-      def self.vps_template(driver)
-        # use driver
-        domain_xml = LibXML::XML::Node.new('domain')
-        domain_xml['type'] = 'openvz'
-        domain_xml['id'] = '135'
-        domain_xml << LibXML::XML::Node.new('name', '')
-        domain_xml << LibXML::XML::Node.new('uuid', '')
-        domain_xml << LibXML::XML::Node.new('vcpu', '1')
-        
-        devices_xml = LibXML::XML::Node.new('devices')
-        
-        filesystem_xml = LibXML::XML::Node.new('filesystem')
-        filesystem_xml['type'] = 'template'
-        
-        source_xml = LibXML::XML::Node.new('source')
-        source_xml['name'] = 'debian-6.0-x86'
-        
-        target_xml = LibXML::XML::Node.new('target')
-        target_xml['dir'] = '/'
-        
-        filesystem_xml << source_xml
-        filesystem_xml << target_xml
-        
-        interface_xml = LibXML::XML::Node.new('interface')
-        interface_xml['type'] = 'ethernet'
-        
-        ip_xml = LibXML::XML::Node.new('ip')
-        ip_xml['address'] = '10.10.1.135'
-        
-        interface_xml << ip_xml
-        
-        devices_xml << filesystem_xml
-        devices_xml << interface_xml
-        
-        domain_xml << devices_xml
-        domain_xml << LibXML::XML::Node.new('memory', '0')
-        
-        os_xml = LibXML::XML::Node.new('os')
-        os_xml << LibXML::XML::Node.new('type', 'exe')
-        os_xml << LibXML::XML::Node.new('init', '/sbin/init')
-        
-        domain_xml << os_xml
-        domain_xml
       end
     end
   end
