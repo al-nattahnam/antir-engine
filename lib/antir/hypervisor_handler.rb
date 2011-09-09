1b3bbbab7c2bc3939de62cd9867c696b3fc6b07b
Fernando Alonso <krakatoa1987@gmail.com>
2011-09-09 12:54:45 -0300

    Implemented Hypervisor usage on VPS class

--- lib/antir/hypervisor_handler.rb
+++ lib/antir/hypervisor_handler.rb
@@ -1,42 +1,28 @@
 require 'libvirt'
 require 'singleton'
-# require 'forwardable'
+ require 'forwardable'
 require 'antir/hypervisor/domain_handler'
 
 module Antir
   module Hypervisor
+
     #@@hypervisors = [:openvz, :xen]
     HYPERVISOR = 'openvz'
     @@connection = Libvirt::connect("#{HYPERVISOR}:///system")
     @@domain_handler = Antir::Hypervisor::DomainHandler.instance
     @@domain_handler.connection = @@connection
 
+    extend Forwardable
+    def_delegators :@@domain_handler, :find, :max_id
+
     def domains
       @@domain_handler #.domains
     end
 
-    # delegate max_domain_id, find, create.... a domain_handler
+    # delegate max_domain_id, find, create, stop, etc.... a domain_handler
     #def max_domain_id
     #  @@domain_handler.max_id
     #end
-
-#    def find(id)
-#      #xml = LibXML::XML::Parser.string(dom.xml).parse
-#      #domain_xml = xml.find('//domain')[0]
-#
-#      #vps = Antir::Container::VPS.new
-#      #vps.id = domain_xml['id']
-#      #vps.uuid = domain_xml.find('//uuid')[0].content
-#      #vps.name = domain_xml.find('//name')[0].content
-#      #vps.ip = domain_xml.find('//devices/interface/ip')[0]['address']
-#      #vps
-#    end
-#
-#    def create(xml)
-#      # validate
-#      #   name presence
-#      @@connection.domains.create(xml)
-#    end
   end
 
   class HypervisorHandler
